8fb0541e6e5058b264f55cadcec59e60292031fd
leadfyweb <leadfyweb@gmail.com>
2026-02-28 19:46:36 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+nvm install node
+nvm use node
+
+npm install
+npm install -D vite@latest @vitejs/plugin-react@latest
+
+npm audit fix
+npm audit --audit-level=high
+
+curl -o leadyfyscript-move-import-css.js https://raw.githubusercontent.com/leadfyweb/scripts/main/move-import-css.js
+
+node leadyfyscript-move-import-css.js src/index.css
+rm leadyfyscript-move-import-css.js
+
+git push origin HEAD:deploy --force
+
+echo "Deploy bem sucedido"
